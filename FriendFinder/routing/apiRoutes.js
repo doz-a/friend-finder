9c870f806a23e89f1da20837de4bce1812522730
Miguel Delos Santos <mendoza.d.santos@gmail.com>
2020-01-17 11:18:13 -0800

    apiRoutes.js correctly pushes results to survey modal

--- FriendFinder/routing/apiRoutes.js
+++ FriendFinder/routing/apiRoutes.js
@@ -4,6 +4,36 @@ module.exports = function (app) {
         return res.json(friends);
     });
 
+    // best match 
+    app.post("/api/friends", function (req, res) {
+        var bestMatch = {
+            name: "",
+            photo: "",
+            scoreDifference: 100
+        }
+        var userData = req.body;
+        var userScores = userData.scores;
+        var difference = 0;
+        for (let i = 0; i < friends.length; i++) {
+            const element = friends[i];
+            var difference = 0;
+            console.log(element.name);
+            for (let j = 0; j < element.scores.length; j++) {
+                const scores = element.scores[j];
+                var userScore = userScores[j];
+                difference += Math.abs(parseInt(scores) - parseInt(userScore))
+            }
+            // Best Match 
+            if (bestMatch.scoreDifference > difference) {
+                bestMatch.name = element.name;
+                bestMatch.photo = element.photo;
+                bestMatch.scoreDifference = difference
+            }
+        }
+        friends.push(userData);
+        res.json(bestMatch);
+    });
+
     // Displays a single character, or returns false
     app.get("/api/characters/:character", function (req, res) {
         var chosen = req.params.character;
