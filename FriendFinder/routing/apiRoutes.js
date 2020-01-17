1c29c0a423b80f72d787f1174d7b7f5d69ae1d3c
Miguel Delos Santos <mendoza.d.santos@gmail.com>
2020-01-17 10:18:09 -0800

    html added, html routes from server added

---
+++ FriendFinder/routing/apiRoutes.js
@@ -0,0 +1,18 @@
+app.get("/api/friends", function (req, res) {
+    return res.json(characters);
+});
+
+// Displays a single character, or returns false
+app.get("/api/characters/:character", function (req, res) {
+    var chosen = req.params.character;
+
+    console.log(chosen);
+
+    for (var i = 0; i < characters.length; i++) {
+        if (chosen === characters[i].routeName) {
+            return res.json(characters[i]);
+        }
+    }
+
+    return res.json(false);
+});
