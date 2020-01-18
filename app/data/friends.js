750fe105b8ed3bd685af24acd466eedb78d17797
Miguel Delos Santos <mendoza.d.santos@gmail.com>
2020-01-18 14:27:02 -0800

    heroku deployment test

---
+++ app/data/friends.js
@@ -0,0 +1,100 @@
+var friends = [
+    {
+        "name": "Pancakes",
+        "photo": "http://placekitten.com/300/302",
+        "scores": [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "Butters",
+        "photo": "http://placekitten.com/301/302",
+        "scores": [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        "name": "Cake",
+        "photo": "http://placekitten.com/303/302",
+        "scores": [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        "name": "Potato",
+        "photo": "http://placekitten.com/303/296",
+        "scores": [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"
+        ]
+    },
+    {
+        "name": "Cotton Candy",
+        "photo": "http://placekitten.com/299/296",
+        "scores": [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+        ]
+    },
+    {
+        "name": "Barbecue",
+        "photo": "http://placekitten.com/298/296",
+        "scores": [
+            "4",
+            "4",
+            "2",
+            "3",
+            "2",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    }
+]
+
+module.exports = friends
